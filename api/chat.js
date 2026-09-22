347bbf3b0d1b1523435c171a4a7ec537d1fbf1b3
chauchung2007a-rgb <chauchung2007a@gmail.com>
2026-09-22 20:02:54 +0700

    Change model from llama-3.3 to llama-3.1

--- api/chat.js
+++ api/chat.js
@@ -350,7 +350,7 @@ ${projectContext}
           },
 
           body: JSON.stringify({
-            model: "llama-3.3-70b-versatile",
+            model: "llama-3.1-8b-instant",
 
             messages: [
               {
